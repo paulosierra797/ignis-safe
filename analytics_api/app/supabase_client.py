b4ef0ad1b14b314895d33c2194b0b168cf2602bf
Paulo <sierraravepaulo@gmail.com>
2026-07-05 00:17:30 +0800

    render

--- analytics_api/app/supabase_client.py
+++ analytics_api/app/supabase_client.py
@@ -2,7 +2,7 @@ import os
 from supabase import create_client
 
 SUPABASE_URL = os.environ["SUPABASE_URL"]
-SUPABASE_SERVICE_KEY = os.environ["SUPABASE_SERVICE_KEY"]
+SUPABASE_SERVICE_KEY = os.environ["SUPABASE_SERVICE_ROLE_KEY"]
 
 supabase = create_client(
     SUPABASE_URL,
